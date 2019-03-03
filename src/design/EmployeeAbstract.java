d334c5acfd0ae071c5026e986c671837b4a7bac1
jenniferstephen <jenniferkasi234@gmail.com>
2019-03-03 01:54:55 -0500

    Revert "Design"
    This reverts commit 1f5594c2

---
+++ src/design/EmployeeAbstract.java
@@ -0,0 +1,16 @@
+package design;
+
+public class EmployeeAbstract {
+
+    public void EmplyoeeVaction() {
+        System.out.println("Employee's Can take one week of vacation");
+
+    }
+
+    public void EmplyoeePerformance() {
+
+        System.out.println("Emplyoee bonus is gauged on Performance and Salary");
+    }
+
+
+}
