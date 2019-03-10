4d1df341d3863263d8e19ca79d5e60de62b60b68
jenniferstephen <jenniferkasi234@gmail.com>
2019-03-10 15:37:24 -0400

    Midterm: p2

--- src/json/parser/CnnAPI.java
+++ src/json/parser/CnnAPI.java
@@ -53,8 +53,46 @@ public class CnnAPI {
 	   Store into choice of your database and retrieve.
 
      */
-      
+    public static void main(String[] args) throws IOException {
+        String sURL = "http://newsapi.org/v2/top-headlines?sources=cnn&apiKey=0d9e35dfa3c140aab8bf9cdd70df957f";
+        Articles art = null;
+        List<Articles> artList = new ArrayList<>();
+        URL url = new URL(sURL);
+        URLConnection request = url.openConnection();
+        JsonArray jsonArray = null,jsonArry2=null;
+        JsonObject rootObj=null;
+        JsonParser jp = new JsonParser();
+        JsonElement root = jp.parse(new InputStreamReader((InputStream) request.getContent()));
+        if (root instanceof JsonObject) {
+            rootObj = root.getAsJsonObject();
+            jsonArry2 = rootObj.getAsJsonArray("articles");
+        } else if (root instanceof JsonArray) {
+            jsonArray = root.getAsJsonArray();
+        }
+
+        System.out.println(jsonArry2.size());
+        for (int i = 0; i < jsonArry2.size()-1; i++) {
+            try {
+                JsonObject jsonobject = jsonArry2.get(i).getAsJsonObject();
+                String author = jsonobject.get("author").toString();
+                String title = jsonobject.get("title").toString();
+                String description = jsonobject.get("description").toString();
+                String urll = jsonobject.get("url").toString();
+                String urlToImage = jsonobject.get("urlToImage").toString();
+                String publishedAt = jsonobject.get("publishedAt").toString();
+                String content = jsonobject.get("content").toString();
+                System.out.println(author+" "+title+" "+description+" "+urll+" "+ urlToImage+" "+ publishedAt+" "+ content);
+            }catch(Exception ex){
+
+            }
+            for(Articles entry:artList){
+                System.out.println(entry.getAuthor()+"\t "+entry.getTitle()+"\t "+entry.getDescription()+"\t"+entry.getUrl()+" "+
+                        entry.getUrlToImage()+"\t"+ entry.getPublishedAt()+"\t "+ entry.getContent());
+            }
+        }
 
 
 
-}
+
+
+    }
